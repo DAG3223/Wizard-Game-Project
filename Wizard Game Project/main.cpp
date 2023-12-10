a5fe5050dd291877902f72f448e2149b89e234a2
Dylan Garza <DAG.Gamer999@gmail.com>
2023-12-09 21:46:42 -0600

    Raylib boilerplate

---
+++ Wizard Game Project/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+#include "raylib.h"
+#include "raymath.h"
+
+int main() {
+	InitWindow(800, 800, "Wizard Game");
+
+
+	while (!WindowShouldClose()) {
+
+	}
+
+
+	CloseWindow();
+	return 0;
+}
