039d84cd92e06972ebdbdfd519144248da74405b
Dylan Garza <DAG.Gamer999@gmail.com>
2023-12-24 13:26:42 -0600

    Added more basic functionality from doc I made in free time at school: https://docs.google.com/document/d/1x9Cdxj-S-XlqZP11B4veZLkXjJeTvv8l-kGmTC2Lb4I/edit?usp=sharing

--- Wizard Game Project/main.cpp
+++ Wizard Game Project/main.cpp
@@ -93,22 +93,122 @@ struct BlockInfo {
 	Texture2D texture;
 };
 
-//Hitboxes for blocks
-class Tile {
-	uint8_t size{32};
-	Tile* block_root = this;
+class BlockInfoArray {
+	static std::vector<BlockInfo> data;
+public:
+	//UNIMPLEMENTED
+	static void load(const char* file) {
+		//how to store data?
+		//formatted strings?
 
+
+	}
+
+	static BlockInfo* fetch(uint16_t id) {
+		return &data.at(id);
+	}
 };
 
-//matrix of tiles
-class Tilemap {
+//Hitboxes for blocks, 9 bytes
+struct Tile {
+	//Tile* block_root = this; //"this" indicates that the tile is the block's root. otherwise, set the ptr to the block root 
+	uint16_t rootX = 0; //2 bytes
+	uint16_t rootY = 0; //2 bytes
+	uint16_t block_id = -1; //2 bytes, only set for block source tile
 
+	static const uint8_t size = 32; // 1 byte
+	bool occupied = false; //1 byte, block inhabits area but may not be the source of the block
+	bool solid = false;   //1 byte
 };
 
 //Block
 class Block {
-
+	BlockInfo* info;
+	uint16_t hp;
 };
+
+//Matrix of tiles
+class Tilemap {
+	typedef std::unique_ptr<Tile> uptr_Tile;
+	
+	std::vector<std::vector<uptr_Tile>> map;
+public:
+
+	//Tilemap width & height = pow(2, power), min 1, max 16
+	Tilemap(uint8_t power) {
+		int size = pow(2, Clamp(power, 1, 16));
+		
+		map.resize(size);
+		for (auto& row : map) {
+			row.resize(size);
+		}
+	}
+
+	//BASIC FUNCTIONALITY:
+	//~~~~~~~~~~~~~~~~~~~~
+	
+	//returns tile ptr ref at provided indices
+	uptr_Tile& at(uint16_t x, uint16_t y) {
+		return map.at(y).at(x);
+	}
+
+	//returns if a tile is occupied
+	bool empty(uint16_t x, uint16_t y) {
+		return at(x, y)->occupied; // if a tile is occupied, then it is not empty
+	}
+
+	//set targeted tile's block id
+	void set_BlockID(uint16_t x, uint16_t y, uint16_t id) {
+		at(x, y)->block_id = id;
+	}
+		
+	//set targeted tile's occupied flag
+	void set_occupied(uint16_t x, uint16_t y, bool occ) {
+		at(x, y)->occupied = occ;
+	}
+		
+	//set targeted tile's solid flag
+	void set_solid(uint16_t x, uint16_t y, bool s) {
+		at(x, y)->solid = s;
+	}
+		
+
+	//~~~~~~~~~~~~~~~~~~~~
+
+
+
+	//
+	void Place(uint16_t id, uint16_t x, uint16_t y) {
+		if (!empty(x, y)) return;
+		BlockInfo* info = BlockInfoArray::fetch(id);
+
+		//Rectangle region = { x * Tile::size, y * Tile::size, info->tile_width * Tile::size, info->tile_height * Tile::size };
+
+
+		//transform block tile region to rectangle
+		//transform tilemap into rectangle
+		//if returned rectangle is not block, then consider out of bounds
+		//Rectangle GetCollisionRec(Rectangle rec1, Rectangle rec2);
+	}
+
+	//
+	void Destroy(uint16_t x, uint16_t y) {
+		if (empty(x, y)) return;
+
+
+
+	}
+
+	//
+	void Damage(uint32_t damage, uint16_t x, uint16_t y) {
+		if (empty(x, y)) return;
+
+
+
+	}
+};
+
+
 
 
 
@@ -116,11 +216,17 @@ class Block {
 
 int main() {
 	InitWindow(800, 800, "Wizard Game");
+	SetTargetFPS(60);
 
-	//Tilemap map(1, 1);
+	Tilemap foreground(4); // 4x4, 16 
 
 	while (!WindowShouldClose()) {
+		
+		//draw
+		BeginDrawing();
+		ClearBackground(DARKBLUE);
 
+		EndDrawing();
 	}
 
 
